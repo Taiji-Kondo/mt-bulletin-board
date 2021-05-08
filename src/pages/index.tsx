3ed5bc673f4c0e8f363e719a9458e8712b52d315
Taiji Kondo <>
2021-05-08 19:17:03 +0900

    SETTING: ルートパスのエイリアス設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
-import Button from '../components/Button'
+import Button from '@/components/Button'
 
 export default function Home(): JSX.Element {
   return (
