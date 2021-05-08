620df281d2168b2276de48f3aa49355692383db3
Taiji Kondo <>
2021-05-08 16:09:58 +0900

    TEST: huskyのテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home(): JSX.Element {
         </h1>
         <p className="text-red-900">test</p>
         <p className="text-2xl" style={{ display: 'block' }}>hoge</p>
+        <p className="text-2xl" style={{ display: 'block' }}>hoge</p>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
