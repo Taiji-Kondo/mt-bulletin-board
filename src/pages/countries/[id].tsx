ca281f9cca1be12b526b51d689e1f9a982bc810d
Taiji-Kondo <k.taiji1108@gmail.com>
2021-08-30 20:56:52 +0900

    ADD: countryCodeをページのqueryから受け取る

--- src/pages/countries/[id].tsx
+++ src/pages/countries/[id].tsx
@@ -1,26 +1,26 @@
-import { NextPage, NextPageContext } from 'next'
+import { NextPage } from 'next'
 import Link from 'next/link'
 import { CountryItem } from '@/components/country/countryItem'
+import { useRouter } from 'next/router'
 
 type PostPageProps = {
   code: string
 }
 
-const PostPage: NextPage<PostPageProps> = ({ code }) => {
+const PostPage: NextPage<PostPageProps> = () => {
+  const router = useRouter()
+  const { id } = router.query
+
   return (
     <div>
       <h1>POST</h1>
-      <CountryItem codeName={code} />
+      <CountryItem codeName={id as string} />
       <hr />
       <Link href="/">
         <a>Go back</a>
       </Link>
     </div>
   )
-}
-
-PostPage.getInitialProps = ({ query }: NextPageContext) => {
-  return { code: String(query) }
 }
 
 export default PostPage
