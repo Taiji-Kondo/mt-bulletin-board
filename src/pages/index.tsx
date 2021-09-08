1515c6d64a06afc1758461c7dadb49c3c5bcff6b
Taiji-Kondo <k.taiji1108@gmail.com>
2021-09-08 21:35:27 +0900

    CREATE: country一覧ページに飛ぶリンクをトップに作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { VFC, Fragment } from 'react'
 import { BaseLayout } from '@/layouts/BaseLayout'
 import { PageSEO } from '@/layouts/head/PageSEO'
+import Link from 'next/link'
+import { Button } from '@chakra-ui/react'
 
 const Page: VFC = () => {
   return (
@@ -8,6 +10,9 @@ const Page: VFC = () => {
       <BaseLayout>
         <PageSEO seo={{ title: 'top' }} />
         top
+        <Button colorScheme="blue">
+          <Link href={`/countries`}>Country</Link>
+        </Button>
       </BaseLayout>
     </Fragment>
   )
