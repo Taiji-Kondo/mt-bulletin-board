c63176a73d6c7b1cddfbbdbf72d5daf7505a2e39
taiji <k.taiji1108@gmail.com>
2022-07-23 13:49:58 +0900

    feat: Crate SEO base

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,8 +2,15 @@ import 'src/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { BaseSEO } from '@/components/layouts/head/BaseSEO';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <BaseSEO />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
