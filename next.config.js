1b2ae209cbf25f931225b1c495b35999f015df91
taiji <k.taiji1108@gmail.com>
2022-07-23 13:34:59 +0900

    chore: Change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  optimizeFonts: true,
+  pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        destination: '/top',
+        source: '/',
+      },
+    ];
+  },
   swcMinify: true,
 }
 
