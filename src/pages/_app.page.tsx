df367d4c59312d00ed11cc9248ae3c3256bcdb5e
taiji <k.taiji1108@gmail.com>
2022-07-23 14:15:33 +0900

    feat: Add recoil sample

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import 'src/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 import { ErrorBoundary } from '@/components/layouts/ErrorBoundary/ErrorBoundary';
 import { ErrorBoundaryFallback } from '@/components/layouts/ErrorBoundary/ErrorBoundaryFallback';
@@ -8,10 +9,12 @@ import { BaseSEO } from '@/components/layouts/head/BaseSEO';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
-      <BaseSEO />
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <RecoilRoot>
+      <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
+        <BaseSEO />
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </RecoilRoot>
   );
 }
 
