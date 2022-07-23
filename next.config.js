340802cd810f25939778f2ce3dfedca682ac14da
taiji <k.taiji1108@gmail.com>
2022-07-23 12:43:17 +0900

    feat: create-next-app

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizeFonts: true,
-  pageExtensions: ['page.tsx'],
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        destination: '/top',
-        source: '/',
-      },
-    ];
-  },
-};
+  swcMinify: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
