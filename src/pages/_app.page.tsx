d8ef3cb4338afb09c2f0cdfe3468d18f69b5b3ed
taiji <k.taiji1108@gmail.com>
2022-07-23 13:55:06 +0900

    feat: Add ErrorBoundary

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,14 +2,16 @@ import 'src/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { ErrorBoundary } from '@/components/layouts/ErrorBoundary/ErrorBoundary';
+import { ErrorBoundaryFallback } from '@/components/layouts/ErrorBoundary/ErrorBoundaryFallback';
 import { BaseSEO } from '@/components/layouts/head/BaseSEO';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ErrorBoundary FallbackComponent={ErrorBoundaryFallback}>
       <BaseSEO />
       <Component {...pageProps} />
-    </>
+    </ErrorBoundary>
   );
 }
 
