dad8354ba607e5e8443e401a278a4850c1875ed5
taiji <k.taiji1108@gmail.com>
2022-08-12 21:08:58 +0900

    chore: Create ErrorBoundaryFallback

--- src/layouts/ErrorBoundary/ErrorBoundaryFallback.tsx
+++ src/layouts/ErrorBoundary/ErrorBoundaryFallback.tsx
@@ -1,5 +1,13 @@
+import { Button, Text } from '@mantine/core';
 import type { FallbackProps } from 'react-error-boundary';
+import { Heading } from 'tabler-icons-react';
 
-export const ErrorBoundaryFallback = (_?: FallbackProps) => {
-  return <>error</>;
+export const ErrorBoundaryFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  return (
+    <>
+      <Heading>error</Heading>
+      <Text>{error.message}</Text>
+      <Button onClick={resetErrorBoundary}>Reload</Button>
+    </>
+  );
 };
