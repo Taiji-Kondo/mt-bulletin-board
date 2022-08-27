769d909d01f0703e4348a8cb9996fe76783cf1db
taiji <k.taiji1108@gmail.com>
2022-08-28 08:58:41 +0900

    chore: Change urqlClient options

--- src/libs/urqlClient.ts
+++ src/libs/urqlClient.ts
@@ -1,8 +1,12 @@
 import { suspenseExchange } from '@urql/exchange-suspense';
+import type { ClientOptions } from 'urql';
 import { cacheExchange, createClient, dedupExchange, fetchExchange } from 'urql';
 
-export const urqlClient = createClient({
+export const CLIENT_OPTIONS: ClientOptions = {
   exchanges: [dedupExchange, cacheExchange, fetchExchange, suspenseExchange],
+  requestPolicy: 'network-only', // TODO 検証用
   suspense: true,
   url: process.env.NEXT_PUBLIC_SERVER_URL ?? '',
-});
+};
+
+export const urqlClient = createClient(CLIENT_OPTIONS);
