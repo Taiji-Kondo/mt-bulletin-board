2a2b2fb980a7deeeaa16a194417a15951b38afaa
taiji <k.taiji1108@gmail.com>
2022-08-28 08:59:08 +0900

    chore: Set NavLink to TopPage

--- src/pages/top/index.page.tsx
+++ src/pages/top/index.page.tsx
@@ -1,10 +1,23 @@
-import { Title } from '@mantine/core';
+import { Button, Stack, Title } from '@mantine/core';
 import type { NextPageWithLayoutType } from 'next';
+import Link from 'next/link';
 
 import { WithHeaderLayout } from '@/layouts/WithHeaderLayout';
 
 const TopPage: NextPageWithLayoutType = () => {
-  return <Title order={1}>Top</Title>;
+  return (
+    <>
+      <Title order={1}>Top</Title>
+      <Stack>
+        <Link href={'/lift'}>
+          <Button>All Lists</Button>
+        </Link>
+        <Link href={'/user'}>
+          <Button>User</Button>
+        </Link>
+      </Stack>
+    </>
+  );
 };
 
 TopPage.getLayout = (page) => <WithHeaderLayout>{page}</WithHeaderLayout>;
