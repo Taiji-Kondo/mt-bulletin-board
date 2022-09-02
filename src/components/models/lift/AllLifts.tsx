10c4abb833278214da2201c941fa6b84091ca7e6
taiji <k.taiji1108@gmail.com>
2022-09-02 09:02:36 +0900

    feat: Add refresh fuction

--- src/components/models/lift/AllLifts.tsx
+++ src/components/models/lift/AllLifts.tsx
@@ -1,20 +1,30 @@
-import { Button, Stack, Text } from '@mantine/core';
+import { Box, Button, Center, Stack, Text } from '@mantine/core';
 
 import { BaseLink } from '@/components/functional/link/BaseLink';
 import { useAllLiftsQuery } from '@/generated/graphql';
 
 export const AllLifts = () => {
-  const [{ data }] = useAllLiftsQuery();
+  const [{ data }, executeQuery] = useAllLiftsQuery();
+
+  const refresh = () => {
+    console.log('refresh');
+    executeQuery({ requestPolicy: 'network-only', suspense: true });
+  };
 
   if (!data || data.allLifts.length < 0) return <Text>Not found lifts</Text>;
 
   return (
-    <Stack spacing={12}>
-      {data?.allLifts.map((lift) => (
-        <BaseLink href={(path) => path.lift._id(lift.id).$url()} key={lift.id}>
-          <Button variant={'subtle'}>{lift.name}</Button>
-        </BaseLink>
-      ))}
-    </Stack>
+    <Box>
+      <Stack spacing={12}>
+        {data?.allLifts.map((lift) => (
+          <BaseLink href={(path) => path.lift._id(lift.id).$url()} key={lift.id}>
+            <Button variant={'subtle'}>{lift.name}</Button>
+          </BaseLink>
+        ))}
+      </Stack>
+      <Center mt={50}>
+        <Button onClick={refresh}>Refresh</Button>
+      </Center>
+    </Box>
   );
 };
