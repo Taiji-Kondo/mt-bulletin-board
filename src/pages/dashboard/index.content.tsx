4efd4963b15bd426763479a815db54c3ba5f0a89
taiji <k.taiji1108@gmail.com>
2022-09-05 08:29:19 +0900

    feat: Created content page of dashboard

---
+++ src/pages/dashboard/index.content.tsx
@@ -0,0 +1,5 @@
+import { Title } from '@mantine/core';
+
+export const DashboardPageContent = () => {
+  return <Title order={1}>Dashboard</Title>;
+};
--- src/pages/dashboard/index.page.tsx
+++ src/pages/dashboard/index.page.tsx
@@ -1,10 +1,9 @@
-import { Title } from '@mantine/core';
-
 import { WithHeaderLayout } from '@/layouts/WithHeaderLayout';
+import { DashboardPageContent } from '@/pages/dashboard/index.content';
 import type { NextPageWithLayoutType } from '@/types/NextLayoutType';
 
 const DashboardPage: NextPageWithLayoutType = () => {
-  return <Title order={1}>Dashboard</Title>;
+  return <DashboardPageContent />;
 };
 
 DashboardPage.getLayout = (page) => <WithHeaderLayout>{page}</WithHeaderLayout>;
