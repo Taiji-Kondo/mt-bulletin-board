6a20c36d04f6a8dbc5de2f326c63d8ae4752a81c
taiji <k.taiji1108@gmail.com>
2022-09-05 08:26:28 +0900

    feat: Created content page of user

---
+++ src/pages/user/index.content.tsx
@@ -0,0 +1,5 @@
+import { Title } from '@mantine/core';
+
+export const UserPageContent = () => {
+  return <Title order={1}>User</Title>;
+};
--- src/pages/user/index.page.tsx
+++ src/pages/user/index.page.tsx
@@ -1,10 +1,9 @@
-import { Title } from '@mantine/core';
-
 import { WithHeaderLayout } from '@/layouts/WithHeaderLayout';
+import { UserPageContent } from '@/pages/user/index.content';
 import type { NextPageWithLayoutType } from '@/types/NextLayoutType';
 
 const UserPage: NextPageWithLayoutType = () => {
-  return <Title order={1}>User</Title>;
+  return <UserPageContent />;
 };
 
 UserPage.getLayout = (page) => <WithHeaderLayout>{page}</WithHeaderLayout>;
