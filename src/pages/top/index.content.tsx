48c57a707380a7863e6d21f033b3c222f2b68bfd
taiji <k.taiji1108@gmail.com>
2022-10-30 17:14:05 +0900

    fix: Fix next link to without a tag

--- src/pages/top/index.content.tsx
+++ src/pages/top/index.content.tsx
@@ -9,13 +9,13 @@ export const TopPageContent = () => {
       <Title order={1}>Top</Title>
       <Stack>
         <BaseLink href={(path) => path.lift.$url()}>
-          <Button component={'a'}>All Lists</Button>
+          <Button>All Lists</Button>
         </BaseLink>
         <BaseLink href={(path) => path.user.$url()}>
-          <Button component={'a'}>User</Button>
+          <Button>User</Button>
         </BaseLink>
         <BaseLink href={(path) => path.dashboard.$url()}>
-          <Button component={'a'}>Dashboard</Button>
+          <Button>Dashboard</Button>
         </BaseLink>
 
         <BaseExternalLink href={'https://www.google.com/'}>Google</BaseExternalLink>
